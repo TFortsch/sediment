2d293999784b48385cdb64b849c4e77e94871914
David Kahler <david.m.kahler@gmail.com>
2024-05-30 11:23:30 -0400

    Consistent with GEE, produces RGB.

--- sediment_indices.py
+++ sediment_indices.py
@@ -1,4 +1,8 @@
 # Pull images and determine sediment indices for AOIs
+# Remember to update ee.Initialize() with authenticated project - LINE 18
+
+# Initial attempt in GEE:
+# https://code.earthengine.google.com/77a2e41436a615fe1c04c21fec61e9d0
 
 # https://developers.google.com/earth-engine/guides/python_install
 # pip install earthengine-api --upgrade
@@ -9,7 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #ee.Authenticate(force=True)
-# Token generated with all permissions 29 May 2024
+# Token generated with all permissions 29 May 2024, DMK
 ee.Authenticate()
 ee.Initialize(project = 'ee-davidmkahler-limpopo')
 
@@ -21,6 +25,7 @@ balule = ee.Geometry.Polygon(
           [31.72093418705564, -24.05800214694256],
           [31.71991494762998, -24.054984679571938]], None, False)
 balule = balule.reproject(crs='EPSG:32736', scale=1)
+# the analysis area is transformed into a UTM rectangle in the .sampleRectangle/.getInfo phases
 
 # Define source data
 image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')\
@@ -29,10 +34,10 @@ image = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')\
 
 # CRS is not the same.  We need to set defaults.
 # proj = image.first().select('B2').projection() # EPSG:32656, UTM zone 56N (Siberia?), for reference, balule is EPSG:4326, lat/lon WGS 84
-proj = balule.projection()
+# proj = balule.projection()
 
 # Should be built into a function from here
-mosaic = image.median().reproject(crs='EPSG:32736', scale=1)
+mosaic = image.median().reproject(crs='EPSG:32736', scale=1) # This allows us to set the resolution.
 # G = mosaic.select('B3')
 # R = mosaic.select('B4')
 # NIR = mosaic.select('B8')
@@ -50,38 +55,40 @@ dekker = ((mosaic.select('B3')).add(mosaic.select('B4'))).divide(2)
 water = ndwiG.gte(-0.02)
 wetDekker = dekker.updateMask(water)
 
-# Find means
-# https://gis.stackexchange.com/questions/350771/moving-from-earth-engine-image-to-array-for-use-in-sklearn/351177#351177
+# Export arrays
+# https://gist.github.com/jdbcode/f4d56d72f7fc5beeaa3859999b1f5c3d
+# https://gist.github.com/jdbcode/f4d56d72f7fc5beeaa3859999b1f5c3d?permalink_comment_id=3355627#gistcomment-3355627
 band_arrs = mosaic.sampleRectangle(region=balule)
 
 # Get individual band arrays.
+band_arr_b2 = band_arrs.get('B2')   # Blue
 band_arr_b3 = band_arrs.get('B3')   # Green
 band_arr_b4 = band_arrs.get('B4')   # Red
 band_arr_b8 = band_arrs.get('B8')   # NIR
 band_arr_b11 = band_arrs.get('B11') # SWIR
 
 # Transfer the arrays from server to client and cast as np array.
-np_arr_b3 = np.array(band_arr_b3.getInfo())
-np_arr_b4 = np.array(band_arr_b4.getInfo())
-np_arr_b5 = np.array(band_arr_b5.getInfo())
-np_arr_b6 = np.array(band_arr_b6.getInfo())
-print(np_arr_b4.shape)
-print(np_arr_b5.shape)
-print(np_arr_b6.shape)
+b2 = np.array(band_arr_b2.getInfo())
+b3 = np.array(band_arr_b3.getInfo())
+b4 = np.array(band_arr_b4.getInfo())
+b8 = np.array(band_arr_b8.getInfo())
+b11 = np.array(band_arr_b11.getInfo())
 
+# To view/verify region:
+#print(b2.shape)
+#print(b3.shape)
+#print(b4.shape)
+#plt.imshow(b3, interpolation='nearest')
+#plt.show()
+
+# Produce RGB image
 # Expand the dimensions of the images so they can be concatenated into 3-D.
-np_arr_b4 = np.expand_dims(np_arr_b4, 2)
-np_arr_b5 = np.expand_dims(np_arr_b5, 2)
-np_arr_b6 = np.expand_dims(np_arr_b6, 2)
-print(np_arr_b4.shape)
-print(np_arr_b5.shape)
-print(np_arr_b6.shape)
-
+np_arr_b4 = np.expand_dims(b4, 2)
+np_arr_b3 = np.expand_dims(b3, 2)
+np_arr_b2 = np.expand_dims(b2, 2)
 # Stack the individual bands to make a 3-D array.
-rgb_img = np.concatenate((np_arr_b6, np_arr_b5, np_arr_b4), 2)
-print(rgb_img.shape)
-
+rgb_img = np.concatenate((np_arr_b4, np_arr_b3, np_arr_b2), 2)
 # Scale the data to [0, 255] to show as an RGB image.
-rgb_img_test = (255*((rgb_img - 100)/3500)).astype('uint8')
-plt.imshow(rgb_img_test)
+rgb_img = (255*((rgb_img)/3000)).astype('uint8')
+plt.imshow(rgb_img)
 plt.show()
